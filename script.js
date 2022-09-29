25c582f44a0040871bf67b0c30a31a8f1fa142f5
Doppie22 <dvopstal05@gmail.com>
2022-09-29 15:46:44 +0200

    klaar

--- script.js
+++ script.js
@@ -7,34 +7,33 @@ function onvoldoendes(){
     waitForElm('.grade').then((elm) => {
             
         var x = document.body.getElementsByClassName("grade");
-        console.log(x);
+        // console.log(x);
         len = x.length;
-        console.log(len);
+        // console.log(len);
         let i = 0;
         for (i; i<=len; i++)
         {
-            console.log(i);
+            // console.log(i);
             var cijfer = x[i].getAttribute('title');
-            console.log(cijfer);
-            let output = parseFloat(cijfer.replace(/,/g, ''), 2);
-            console.log(output);
-            if (output <= 55 && cijfer !== "") {
-                console.log("onvoldeonde");
-                x[i].classList.add("onvoldoende")
+            // console.log("cijfer"+cijfer);
+            let output = parseFloat(cijfer.replace(/,/g, '.'), 2);
+            // console.log("comma weg"+output);
+            // console.log("class"+x[i].className)
+            if (output <= 5.5 && cijfer !== "" && output != 0) {
+                // console.log("onvoldeonde");
+                x[i].classList.add("onvoldoende");
             }        
             else if (cijfer === ""){
-                console.log("empty");
+                // console.log("empty");
             }
             else{
-                console.log("voldeonde");
+                // console.log("voldeonde");
             }
         } 
 
 
     });
 }
-
-
 
 function waitForElm(selector) {
     return new Promise(resolve => {
@@ -59,20 +58,14 @@ function waitForElm(selector) {
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
-    // fired when a mutation occurs
-    // console.log(mutations, observer);
-    // ...
-    console.log(mutations.length);
+    // console.log(mutations.length);
     if (mutations.length > 750){
-        console.log("pagina switch");
+        // console.log("pagina switch");
         onvoldoendes();
     }
 });
 
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
 observer.observe(document, {
   subtree: true,
   attributes: true
-  //...
 });
