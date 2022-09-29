096b15faf46c1f16b9bee3adc901aa88f4e3c610
Doppie22 <dvopstal05@gmail.com>
2022-09-29 14:58:41 +0200

    gelijk

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-$(function () {
-    setTimeout(() => { 
+waitForElm('.grade').then((elm) => {
         
     var x = document.body.getElementsByClassName("grade");
     console.log(x);
@@ -26,6 +25,25 @@ $(function () {
     } 
 
 
-}, 2000);
-
 });
+
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
