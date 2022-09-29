7f7b480c164df0cdbe3867bf4a14801ed032ec12
Doppie22 <dvopstal05@gmail.com>
2022-09-29 15:22:24 +0200

    werkt alleen niet met gemiddelds

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-waitForElm('.grade').then((elm) => {
-        
-    var x = document.body.getElementsByClassName("grade");
-    console.log(x);
-    len = x.length;
-    console.log(len);
-    let i = 0;
-    for (i; i<=len; i++)
-    {
-        console.log(i);
-        var cijfer = x[i].getAttribute('title');
-        console.log(cijfer);
-        let output = parseFloat(cijfer.replace(/,/g, ''), 2);
-        console.log(output);
-        if (output <= 55 && cijfer !== "") {
-            console.log("onvoldeonde");
-            x[i].classList.add("onvoldoende")
-        }        
-        else if (cijfer === ""){
-            console.log("empty");
-        }
-        else{
-            console.log("voldeonde");
-        }
-    } 
+window.onload = function() {
+    onvoldoendes();
+  };
 
 
-});
+function onvoldoendes(){
+    waitForElm('.grade').then((elm) => {
+            
+        var x = document.body.getElementsByClassName("grade");
+        console.log(x);
+        len = x.length;
+        console.log(len);
+        let i = 0;
+        for (i; i<=len; i++)
+        {
+            console.log(i);
+            var cijfer = x[i].getAttribute('title');
+            console.log(cijfer);
+            let output = parseFloat(cijfer.replace(/,/g, ''), 2);
+            console.log(output);
+            if (output <= 55 && cijfer !== "") {
+                console.log("onvoldeonde");
+                x[i].classList.add("onvoldoende")
+            }        
+            else if (cijfer === ""){
+                console.log("empty");
+            }
+            else{
+                console.log("voldeonde");
+            }
+        } 
+
+
+    });
+}
+
 
 
 function waitForElm(selector) {
@@ -47,3 +55,24 @@ function waitForElm(selector) {
         });
     });
 }
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    // console.log(mutations, observer);
+    // ...
+    console.log(mutations.length);
+    if (mutations.length > 750){
+        console.log("pagina switch");
+        onvoldoendes();
+    }
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+  //...
+});
