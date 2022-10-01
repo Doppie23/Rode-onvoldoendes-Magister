ab6c0dd3b0463fae44379ebe0adaf7a6105ce15b
Doppie22 <dvopstal05@gmail.com>
2022-10-01 14:11:42 +0200

    alleen spiler als nieuw cijfer

--- spoilertag/spoilertag.js
+++ spoilertag/spoilertag.js
@@ -1,32 +1,42 @@
-waitForElm('.last-grade').then((elm) => {
-    // localStorage.setItem('laatste-cijfer', '6,5')
-    
-    var x = document.body.getElementsByClassName("cijfer");
-    var cijfer = x[0].textContent;
-    console.log(cijfer)
-    
-    var oudecijfer = localStorage.getItem('laatste-cijfer')
-    console.log(oudecijfer)
+window.onload = function() {
+    spoiler();
+};
 
-    if (oudecijfer !== cijfer){
-        alert('NIEUW CIJFER')
-        var x = document.body.getElementsByClassName("cijfer");
-        console.log(x)
-        var cijfer = x[0].textContent;
-        localStorage.setItem('laatste-cijfer', cijfer)
-        console.log("cijferaanps" + cijfer)
-        x[0].classList.add("spoiler");
+function spoiler(){
+    waitForElm('.last-grade').then((elm) => {
+        // localStorage.setItem('laatste-cijfer', '6,5')
         
-        var x = document.body.getElementsByClassName("omschrijving");
-        console.log(x)
-        x[0].classList.add("spoiler");
-    
-        var x = document.body.getElementsByClassName("last-grade");
-        console.log(x)
-        x[0].classList.add("spoilerbg"); 
-    }
+        var x = document.body.getElementsByClassName("cijfer");
+        var cijfer = x[0].textContent;
+        console.log(cijfer)
+        
+        var oudecijfer = localStorage.getItem('laatste-cijfer')
+        console.log(oudecijfer)
 
-}),
+        if (oudecijfer !== cijfer){
+            alert('NIEUW CIJFER')
+            var x = document.body.getElementsByClassName("cijfer");
+            console.log(x)
+            var cijfer = x[0].textContent;
+            localStorage.setItem('laatste-cijfer', cijfer)
+            console.log("cijferaanps" + cijfer)
+            x[0].classList.add("spoiler");
+            
+            var x = document.body.getElementsByClassName("omschrijving");
+            console.log(x)
+            x[0].classList.add("spoiler");
+        
+            var x = document.body.getElementsByClassName("last-grade");
+            console.log(x)
+            x[0].classList.add("spoilerbg"); 
+        }
+
+    });
+};
+
+window.onhashchange = function() {
+    spoiler();
+};
 
 function waitForElm(selector) {
     return new Promise(resolve => {
