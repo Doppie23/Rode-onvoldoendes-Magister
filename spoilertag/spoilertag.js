990102b08d45bedc351d3d0d1cb1827636c78df5
Doppie22 <dvopstal05@gmail.com>
2022-10-01 14:30:59 +0200

    spoilertag

--- spoilertag/spoilertag.js
+++ spoilertag/spoilertag.js
@@ -1,38 +1,26 @@
-window.onload = function() {
-    spoiler();
-};
-
-function spoiler(){
+function spoiler(){    
     waitForElm('.last-grade').then((elm) => {
         // localStorage.setItem('laatste-cijfer', '6,5')
         
+        
         var x = document.body.getElementsByClassName("cijfer");
-        var cijfer = x[0].textContent;
-        console.log(cijfer)
+        console.log(x)
+        x[0].classList.add("spoiler");
         
-        var oudecijfer = localStorage.getItem('laatste-cijfer')
-        console.log(oudecijfer)
+        var x = document.body.getElementsByClassName("omschrijving");
+        console.log(x)
+        x[0].classList.add("spoiler");
 
-        if (oudecijfer !== cijfer){
-            alert('NIEUW CIJFER')
-            var x = document.body.getElementsByClassName("cijfer");
-            console.log(x)
-            var cijfer = x[0].textContent;
-            localStorage.setItem('laatste-cijfer', cijfer)
-            console.log("cijferaanps" + cijfer)
-            x[0].classList.add("spoiler");
-            
-            var x = document.body.getElementsByClassName("omschrijving");
-            console.log(x)
-            x[0].classList.add("spoiler");
+        var x = document.body.getElementsByClassName("last-grade");
+        console.log(x)
+        x[0].classList.add("spoilerbg"); 
         
-            var x = document.body.getElementsByClassName("last-grade");
-            console.log(x)
-            x[0].classList.add("spoilerbg"); 
-        }
-
     });
 };
+
+window.onload = function() {
+    spoiler();
+  };
 
 window.onhashchange = function() {
     spoiler();
