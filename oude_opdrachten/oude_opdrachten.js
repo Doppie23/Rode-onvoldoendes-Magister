6a536450465cc475ca3d809562958e0267155c12
Doppie22 <dvopstal05@gmail.com>
2022-10-07 22:42:23 +0200

    klaar

--- oude_opdrachten/oude_opdrachten.js
+++ oude_opdrachten/oude_opdrachten.js
@@ -1,33 +1,21 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
-    // console.log(mutations);
-    // console.log(mutations.length);
-    // console.log(mutations[0].target);
     const target = mutations[0].target
     if (target.matches('.loading-overlay')){
-        console.log("klaar");
+        // console.log("klaar");
 
         var y = document.getElementsByTagName("tbody");
-        // console.log(y);
         y[0].setAttribute('id', 'tbodyID');
 
-
-
-
         var table = document.getElementById('tbodyID');
-        const x = table.getElementsByTagName('tr')
-        
-        console.log(x);
-        console.log(x[0]);
+        var x = table.getElementsByTagName('tr');
 
         var i = 0
         for(var i=x.length - 1; i >= 0; i--){
-            var naam = x[i].className
-            console.log(naam)
-            console.log(x[i].classList.contains('overdue'));
-            if(x[i].classList.contains('overdue') === true){
-                console.log('rem' + x[i])
+            // console.log(x[i].classList.contains('overdue'));
+            if(x[i].classList.contains('overdue') == true){
+                // console.log('rem' + x[i])
                 x[i].remove()
             }
             else{
