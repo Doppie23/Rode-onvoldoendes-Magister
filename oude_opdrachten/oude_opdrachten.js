f5e10ddcd37b5c84b8b62c1707abfdaeeb230bc1
Doppie22 <dvopstal05@gmail.com>
2022-10-07 22:39:00 +0200

    probleem volgorde array verwijderen

--- oude_opdrachten/oude_opdrachten.js
+++ oude_opdrachten/oude_opdrachten.js
@@ -1,18 +1,47 @@
-waitForElm('.overdue').then((elm) => {
-    console.log('Element is ready');
-    console.log(elm.textContent);
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-    var x = document.body.getElementsByClassName("overdue");
-    len = x.length;
-    console.log(len)
-    let i = 0;
-    for (i; i<=len; i++){
-        console.log(x[i])
-        x[i].remove();
+var observer = new MutationObserver(function(mutations, observer) {
+    // console.log(mutations);
+    // console.log(mutations.length);
+    // console.log(mutations[0].target);
+    const target = mutations[0].target
+    if (target.matches('.loading-overlay')){
+        console.log("klaar");
+
+        var y = document.getElementsByTagName("tbody");
+        // console.log(y);
+        y[0].setAttribute('id', 'tbodyID');
+
+
+
+
+        var table = document.getElementById('tbodyID');
+        const x = table.getElementsByTagName('tr')
+        
+        console.log(x);
+        console.log(x[0]);
+
+        var i = 0
+        for(var i=x.length - 1; i >= 0; i--){
+            var naam = x[i].className
+            console.log(naam)
+            console.log(x[i].classList.contains('overdue'));
+            if(x[i].classList.contains('overdue') === true){
+                console.log('rem' + x[i])
+                x[i].remove()
+            }
+            else{
+                continue
+            }
+        }
+
     }
 });
 
-
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
 
 function waitForElm(selector) {
     return new Promise(resolve => {
@@ -33,6 +62,3 @@ function waitForElm(selector) {
         });
     });
 }
-
-
-// dataAvailable=true
